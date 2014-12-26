9a375230899859b8f73b50ae9abd3e2b7e71c5f2
robertpogue <robert@stadia3d.com>
2014-12-25 19:54:47 -0600

    Re-organize functions as button handlers, behaviors, and utility functions. Add christmas party function.

--- hue.js
+++ hue.js
@@ -1,4 +1,6 @@
-function listLights() {
+//////////////////////////////////////////////////////////
+////              Button handlers                     ////
+function listLightsButton() {
 	var response = hueApi("", "GET", "");
 	var outText = "";
     for(var i=1; i <= len(response); i++) {
@@ -7,45 +9,104 @@ function listLights() {
 	output(outText);
 }
 
-function on() {
-	var response = hueApi(lightNumber() + '/state/', 'PUT', '{"on":true}');
-	output(JSON.stringify(response));
+function onButton() {
+	on(lightNumberBox());
 }
 
-function off() {
-    var response = hueApi(lightNumber() + '/state/', 'PUT', '{"on":false}');
-	output(JSON.stringify(response));
+function offButton() {
+	off(lightNumberBox());
 }
 
-function red() {
-	var response = hueApi(lightNumber() + '/state/', 'PUT', '{"hue":0, "sat":255}')
-	output(JSON.stringify(response));
+function redButton() {
+	red(lightNumberBox());
 }
 
-function green() {
-    var response = hueApi(lightNumber() + '/state/', 'PUT', '{"hue":25500, "sat":255}')
-	output(JSON.stringify(response));
+function greenButton() {
+	green(lightNumberBox());
 }
 
-function blue() {
-	var response = hueApi(lightNumber() + '/state/', 'PUT', '{"hue":46920, "sat":255}')
-	output(JSON.stringify(response));
+function blueButton() {
+	blue(lightNumberBox());
+}
+
+function xmasPartyButton() {
+	for(var i=6; i<=10; i++) {
+			off(i);
+	}
+	sleep(1000);
+	for(var i=0; i<3; i++) {
+		// random
+		for(var r=0; r<10; r++) {
+			for(j=6; j<=10; j++) {
+				if(Math.random() > .5) red(j);
+				else green(j);
+			}
+			sleep(500);
+		}
+		// twirl
+		for(var j=0; j<2; j++) {
+			for(var k=6; k<=10; k++) {
+				red(k);
+				sleep(300);
+				green(k);
+			}
+		}
+		for(var i=6; i<=10; i++) {
+			off(i);
+		}
+	}
+	output("The party's over...");
+}
+
+// returns current value of light button text box
+function lightNumberBox() {
+	var lightNumberBox = document.getElementById("lightNumberBox");
+	if(lightNumberBox.value == "") {
+		alert("Enter light number in text box");
+	}
+	return lightNumberBox.value;
 }
 
 function output(string) {
 	document.getElementById("output").innerHTML = string;
 }
 
-function len(array) {
-	return Object.keys(array).length
+//////////////////////////////////////////////////////////
+//// Behavior methods - independent of user interface ////
+
+function on(light) {
+	var response = hueApi(light + '/state/', 'PUT', '{"on":true}');
+	output(JSON.stringify(response));
 }
 
-function lightNumber() {
-	var lightNumber = document.getElementById("lightNumber");
-	if(lightNumber.value == "") {
-		alert("Enter light number in text box");
-	}
-	return lightNumber.value;
+function off(light) {
+    var response = hueApi(light + '/state/', 'PUT', '{"on":false}');
+	output(JSON.stringify(response));
+}
+
+function red(light) {
+	on(light);
+	var response = hueApi(light + '/state/', 'PUT', '{"hue":0, "sat":255}')
+	output(JSON.stringify(response));
+}
+
+function green(light) {
+	on(light);
+    var response = hueApi(light + '/state/', 'PUT', '{"hue":25500, "sat":255}')
+	output(JSON.stringify(response));
+}
+
+function blue(light) {
+	on(light);
+	var response = hueApi(light + '/state/', 'PUT', '{"hue":46920, "sat":255}')
+	output(JSON.stringify(response));
+}
+
+//////////////////////////////////////////////////////////
+////                Utility functions                 ////
+
+function len(array) {
+	return Object.keys(array).length
 }
 
 // sends http request to the hue api and returns the response as javascript object

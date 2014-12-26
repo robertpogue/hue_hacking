f7d755d2500aa75dfdd5ad2d50c71d3da84e580c
robertpogue <robert@stadia3d.com>
2014-12-25 18:06:01 -0600

    Add red, green, blue buttons.

--- hue.js
+++ hue.js
@@ -17,6 +17,21 @@ function off() {
 	output(JSON.stringify(response));
 }
 
+function red() {
+	var response = hueApi(lightNumber() + '/state/', 'PUT', '{"hue":0, "sat":255}')
+	output(JSON.stringify(response));
+}
+
+function green() {
+    var response = hueApi(lightNumber() + '/state/', 'PUT', '{"hue":25500, "sat":255}')
+	output(JSON.stringify(response));
+}
+
+function blue() {
+	var response = hueApi(lightNumber() + '/state/', 'PUT', '{"hue":46920, "sat":255}')
+	output(JSON.stringify(response));
+}
+
 function output(string) {
 	document.getElementById("output").innerHTML = string;
 }
@@ -43,4 +58,13 @@ function hueApi(path, action, body) {
 	http.send(body);
 	var response = JSON.parse(http.responseText);
 	return response;
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 }
